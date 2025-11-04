89a3733cb71dc3eef7a73f14e6cbf63fbe9773b4
Arjun Ryatt <arjunryatt@hotmail.co.uk>
2025-11-04 14:42:54 +0000

    Developed a script to parse variant info in .VCF file

---
+++ tools/utils/parseVCF.py
@@ -0,0 +1,24 @@
+def parseVCF(filepath_filename):
+
+    variant_list = []
+
+    file = open(filename, 'r')
+
+    for line in file:
+
+        if line.startswith('#'):
+
+            continue
+
+        else:
+
+            chromosome = line.split('\t')[0].replace('chr', '')
+            position = line.split('\t')[1]
+            ref = line.split('\t')[3]
+            alt = line.split('\t')[4]
+
+            variant = f'{chromosome}-{position}-{ref}-{alt}'
+
+        variant_list.append(variant.split('\n')[0])
+
+    return variant_list
