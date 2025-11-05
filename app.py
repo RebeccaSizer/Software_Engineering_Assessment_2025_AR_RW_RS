83ab11d7983aa84927e8aeae529a0a85667900d5
Arjun Ryatt <arjunryatt@hotmail.co.uk>
2025-11-05 10:30:52 +0000

    Developed app.py with modules and utilities

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from tools.utils.parseVCF import parseVCF
 from tools.modules.HGVS_convertion import HGVS_converter
+from tools.modules.detailed_request import get_clinvar_full_info
 import sys
 
 if __name__ == '__main__':
     file = sys.argv[1]
     variant_list = parseVCF(file)
-    print(HGVS_converter(variant_list))
+    vv_dict = HGVS_converter(variant_list)
+
+    for key, value in vv_dict.items():
+
+        clinvar_annotation = []
+
+        clinVar_response = get_clinvar_full_info(value)
+
+        clinvar_annotation.append(clinVar_response['classification'])
+        clinvar_annotation.append(clinVar_response['conditions'])
+        clinvar_annotation.append(clinVar_response['stars'])
+        clinvar_annotation.append(clinVar_response['review_status'])
+
+        vv_dict[key] = clinvar_annotation
+
+    print(vv_dict)
+
+
+
 
 
 
