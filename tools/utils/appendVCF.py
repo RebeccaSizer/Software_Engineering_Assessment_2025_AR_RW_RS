c9cfad1e154a82927e901d84d0886c6f9f656764
Arjun Ryatt <arjunryatt@hotmail.co.uk>
2025-11-05 10:41:37 +0000

    developed appendVCF to annotate .VCF file

--- tools/utils/appendVCF.py
+++ tools/utils/appendVCF.py
@@ -1,26 +1,26 @@
 def appendVCF(file, clinvar_output):
 
-    hgvs = clinvar_output['hgvs']
+    for key, value in clinvar_output.items():
 
-    chromosome = hgvs.split('.')[0][-2:].replace('0', '')
-    position = hgvs.split('.')[2][0:-3]
-    ref = hgvs.split('>')[0][-1]
-    alt = hgvs.split('>')[1]
+        chromosome = key.split('.')[0][-2:].replace('0', '')
+        position = key.split('.')[2][0:-3]
+        ref = key.split('>')[0][-1]
+        alt = key.split('>')[1]
 
-    with open(file, 'r') as f:
+        with open(file, 'r') as f:
 
-        lines = f.readlines()
+            lines = f.readlines()
 
-    for line in lines:
+        for line in lines:
 
-        if (line.split('\t')[0].replace('chr', '') == chromosome and
-            line.split('\t')[1] == position and
-            line.split('\t')[3] == ref and
-            line.split('\t')[4] == alt):
+            if (line.split('\t')[0].replace('chr', '') == chromosome and
+                line.split('\t')[1] == position and
+                line.split('\t')[3] == ref and
+                line.split('\t')[4] == alt):
 
-                line.replace('\n', '\t.....\n'
+                    line.replace('\n', f'\t{value}[0]\n'
 
-    with open(file, "w") as file:
-        file.writelines(lines)
+        with open(file, "w") as file:
+            file.writelines(lines)
 
 
