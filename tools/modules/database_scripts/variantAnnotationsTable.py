cbd3f6fda56bed9829a092358c81a99cd63f2165
Arjun Ryatt <arjunryatt@hotmail.co.uk>
2025-11-08 15:46:23 +0000

    database scripts added to flask_app and tested. database scripts were subsequently developed on this branch due to time constraints. developments are being commited so that further developments can be made on database branch.

--- tools/modules/database_scripts/variantAnnotationsTable.py
+++ tools/modules/database_scripts/variantAnnotationsTable.py
@@ -6,7 +6,40 @@ from ...modules.detailed_request import get_clinvar_full_info
 from ...modules.Entrez import Entrez_fetch_transcript_record
 
 def variantAnnotationsTable(filepath):
+    '''
+    This function creates the sea.db database, if it doesn't already exist.
+    It then creates or updates a table in the database that is populated by variants extracted from .vcf files uploaded
+    by the user on our flask website.
 
+    :params: filepath: This leads to the subdirectory where the .vcf files uploaded by the user are stored.
+                       The subdirectory is called 'data' and is located in the base-directory of this software package.
+                       The filepath is not hardcoded into the script because it is the absolute filepath within the
+                       respective computer that this software package was loaded in.
+
+                 E.g.: '/<path>/<to>/<base>/<directory>/<of>/Software_Engineering_Assessment_2025_AR_RW_RS/data/'
+
+    :output: sea.db database: '/<path>/<to>/<base>/<directory>/<of>/Software_Engineering_Assessment_2025_AR_RW_RS/database/sea.db'
+
+             variant_annotations table:
+             No|HGVS NC_ nomenclature|HGVS NM_ nomenclature|HGVS NP_ nomenclature|Gene symbol|HGNC ID|Classification|Conditions|Star-Rating|Review status
+
+             E.g.:
+
+             1|NC_000017.11:g.45990019G>A|NM_007262.5:c.541G>C|NP_009193.2:p.(Val181Leu)|MAPT|6893|Pathogenic|Unknown|★★|criteria provided, multiple submitters, no conflicts
+             2|NC_000012.12:g.40295535A>T|NM_007262.5:c.541G>C|NP_009193.2:p.(Val181Leu)|LRRK2|18618|Pathogenic|Unknown|★★|criteria provided, multiple submitters, no conflicts
+             3|NC_000012.12:g.40346849C>G|NM_007262.5:c.541G>C|NP_009193.2:p.(Val181Leu)|LRRK2|18618|Pathogenic|Unknown|★★|criteria provided, multiple submitters, no conflicts
+             4|NC_000004.12:g.89835580C>G|NM_007262.5:c.541G>C|NP_009193.2:p.(Val181Leu)|SNCA|11138|Pathogenic|Unknown|★★|criteria provided, multiple submitters, no conflicts
+             5|NC_000019.10:g.41985036A>C|NM_007262.5:c.541G>C|NP_009193.2:p.(Val181Leu)|ATP1A3|801|Pathogenic|Unknown|★★|criteria provided, multiple submitters, no conflicts
+
+    :command: filepath = '/<path>/<to>/<base>/<directory>/<of>/Software_Engineering_Assessment_2025_AR_RW_RS/data/'
+              variantAnnotationsTable(filepath)
+
+              To access database: sqlite3 /<path>/<to>/<base>/<directory>/<of>/Software_Engineering_Assessment_2025_AR_RW_RS/database/sea.db
+
+              To view table: SELECT * FROM variant_annotations;
+    '''
+
+    # create a list of the filepath to all of the .vcf files in the data subdirectory
     vcf_paths = []
 
     for file in os.listdir(filepath):
@@ -58,8 +91,19 @@ def variantAnnotationsTable(filepath):
             stars = clinVar_response['stars']
             review_status = clinVar_response['review_status']
 
-            cursor.execute("""INSERT INTO variant_annotations (variant_NC, variant_NM, variant_NP, gene, HGNC_ID, classification, conditions, stars, review_status)
-                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", (key, transcript, np_change, gene, HGNC_ID, classification, conditions, stars, review_status))
+            cursor.execute("""
+                           INSERT INTO variant_annotations 
+                           (variant_NC, variant_NM, variant_NP, gene, HGNC_ID, classification, conditions, stars, review_status)
+                           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                           ON CONFLICT(variant_NC, variant_NM, variant_NP)
+                           DO UPDATE SET
+                               gene = excluded.gene,
+                               HGNC_ID = excluded.HGNC_ID,
+                               classification = excluded.classification,
+                               conditions = excluded.conditions,
+                               stars = excluded.stars,
+                               review_status = excluded.review_status
+                           """, (key, transcript, np_change, gene, HGNC_ID, classification, conditions, stars, review_status))
 
         # Save (commit) changes and close connection
         conn.commit()
