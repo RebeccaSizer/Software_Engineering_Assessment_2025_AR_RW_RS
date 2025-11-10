26ecc71147bd91776faa8652f5d9e45bfcd5ffc6
Arjun Ryatt <arjunryatt@hotmail.co.uk>
2025-11-10 21:32:37 +0000

    parseVCF developed to also parse .csv files and handle variant lines with an irregular length.

--- tools/utils/parseVCF.py
+++ tools/utils/parseVCF.py
@@ -1,26 +1,72 @@
+import csv
+
 def parseVCF(file):
 
     patient_name = file.split('.')[0].split('/')[-1]
 
     variant_list = []
 
-    file = open(file, 'r')
+    line_number = 0
 
-    for line in file:
+    if file.endswith('.vcf') or file.endswith('.VCF'):
 
-        if line.startswith('#'):
+        lines = open(file, 'r')
 
-            continue
+        for line in lines:
 
-        else:
+            line_number = line_number + 1
 
-            chromosome = line.split('\t')[0].replace('chr', '')
-            position = line.split('\t')[1]
-            ref = line.split('\t')[3]
-            alt = line.split('\t')[4]
+            if line.startswith('#'):
 
-            variant = f'{chromosome}-{position}-{ref}-{alt}'
+                print(f'Variant in line {line_number} from {file.split('/')[-1]} is irregular and was not parsed.')
 
-        variant_list.append(variant.split('\n')[0])
+                continue
+
+            elif len(line.split('\t')) <= 4:
+
+                continue
+
+            else:
+
+                chromosome = line.split('\t')[0].replace('chr', '')
+                position = line.split('\t')[1]
+                ref = line.split('\t')[3]
+                alt = line.split('\t')[4]
+
+                variant = f'{chromosome}-{position}-{ref}-{alt}'
+
+            variant_list.append(variant.split('\n')[0])
+
+
+
+    if file.endswith('.csv') or file.endswith('.CSV'):
+
+        csv_file = open(file, 'r')
+        lines = csv.reader(csv_file)
+
+        for line in lines:
+
+            line_number = line_number + 1
+
+            if line[0].startswith('#'):
+
+                continue
+
+            elif len(line) <= 4:
+
+                continue
+
+            else:
+
+                chromosome = line[0].replace('chr', '')
+                position = line[1]
+                ref = line[3]
+                alt = line[4]
+
+                variant = f'{chromosome}-{position}-{ref}-{alt}'
+
+            variant_list.append(variant)
 
     return patient_name, variant_list
+
+#print(parseVCF('/home/ubuntu/Desktop/ParkCSV/Patient1.csv'))
