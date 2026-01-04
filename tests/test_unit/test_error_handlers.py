b1f159a1e83fa038e070a97653bed34766de666c
aryatt2git <arjunryatt@hotmail.co.uk>
2026-01-04 17:43:27 +0000

    change made to assertion of sqlite3 error pytest in test_error_handlers.py.

--- tests/test_unit/test_error_handlers.py
+++ tests/test_unit/test_error_handlers.py
@@ -304,4 +304,4 @@ def test_sqlite_error_returns_generic_message(exception):
     # Assert that a message is returned
     assert msg is not None
     # Assert that the message indicates a database problem
-    assert "something wrong with the database" in msg.lower()
+    assert "something went wrong while accessing the database" in msg.lower()
